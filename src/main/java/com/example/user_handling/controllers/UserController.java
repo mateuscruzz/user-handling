90e673823a88aa0530f68860cd853a610c2f10f8
mateus <mateus.cruz14m@gmail.com>
2024-08-22 14:17:27 -0300

    UserDao & ViewUsers page

--- src/main/java/com/example/user_handling/controllers/UserController.java
+++ src/main/java/com/example/user_handling/controllers/UserController.java
@@ -1,15 +1,26 @@
 package com.example.user_handling.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.user_handling.entities.User;
+import com.example.user_handling.service.UserService;
 
 @Controller
 public class UserController {
 
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/getUsers")
+	public String getUsers(Model model) {
+		model.addAttribute("usersList",userService.getAllUsers());
+		return "ViewUsers";
+	}
+	
 	@GetMapping("/addUser")
 	public String addUser(Model model) {
 		model.addAttribute("user", new User());
@@ -17,7 +28,9 @@ public class UserController {
 	}
 	
 	@PostMapping("/saveUser")
-	public void saveUser(User user) {
-		System.out.println("User has came through");
+	public String saveUser(User user) {
+		userService.addUser(user);
+		
+		return "redirect:/getUsers";
 	}
 }
---
+++ src/main/java/com/example/user_handling/service/UserService.java
@@ -0,0 +1,33 @@
+package com.example.user_handling.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.user_handling.dao.UserDao;
+import com.example.user_handling.entities.User;
+
+@Service
+public class UserService{
+	
+	@Autowired
+	private UserDao userDao;
+	
+	public List<User> getAllUsers(){
+		return userDao.findAll();
+	}
+	
+	public User addUser(User user) {
+		return userDao.save(user);
+	}
+	
+	public User editUser(Long id) {
+		User user = userDao.findById(id).orElse(null);
+		return userDao.save(user);
+	}
+	
+	public void deleteUser(Long id) {
+		userDao.deleteById(id);
+	}
+}
